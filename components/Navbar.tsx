0d1870ae1b3750be0eed683237a0459840fced1e
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-01-11 16:11:18 -0500

    Initial

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return (
+        <div>
+            <div class="bg-violet-200">
+                <p>Navbar</p>
+            </div>
+        </div>
+    )
+}
---
+++ twind.config.ts
@@ -0,0 +1,10 @@
+import { Options } from "$fresh/plugins/twind.ts";
+import * as colors from "twind/colors"
+export default {
+  selfURL: import.meta.url,
+  theme: {
+    extend: {
+      colors,
+    },
+  },
+} as Options;
