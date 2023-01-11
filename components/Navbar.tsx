51cdf7f71c89feeec7be9fcb8b440845877aa689
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-01-11 17:07:56 -0500

    Styled navbar
    transition styling:
    
    styling navbar again...
    
    update manifest

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
 export default function Navbar(){
     return (
-        <div>
-            <div class="bg-violet-200">
-                <p>Navbar</p>
+        <>
+            <div class="flex bg-gray-200 h-20 items-center justify-evenly px-8 font-sans font-medium text-lg antialiased">
+                    <div class="flex space-x-4 justify-evenly divide-x-1 divide-gray-500">
+                        <a class="flex justify-center w-20 pl-4 hover:text-indigo-600 transition" href="/">Home</a>
+                        <a class="flex justify-center w-20 pl-4 hover:text-indigo-600 transition" href="/about">About</a>
+                        <a class="flex justify-center w-20 pl-4 hover:text-indigo-600 transition" href="/projects">Projects</a>
+                    </div>
+                    <div class="ml-auto w-25 px-4 py-1 border rounded-lg border-2 border-gray-300 text-white bg-indigo-600 hover:bg-indigo-500 transition">
+                        <a class="flex justify-center transition" href="/contact">Lets Connect</a>
+                    </div>
             </div>
-        </div>
+        </>
     )
 }
---
+++ routes/contact.tsx
@@ -0,0 +1,7 @@
+export default function CoontactPage() {
+    return (
+        <>
+            <h1>Contact Page</h1>
+        </>
+    )
+}
